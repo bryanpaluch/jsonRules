6773ec3340a192ea9b63a17668f93883efbc82b5
Bryan Paluch <bryanpaluch@gmail.com>
2013-02-07 14:45:28 -0500

    added 2 test cases for async catalog fns

--- tests/2.jsonRules.js
+++ tests/2.jsonRules.js
@@ -24,6 +24,55 @@ var then = exampleRule.then;
 var value1= {temp: 60, setTemp: 70, relay: 1};
 var value2= {temp: 71, setTemp: 70, relay: 1};
 
+var catalogOperandRule = {
+  ifs : [
+    { operands: [ '_object.temp', '_object.setTemp'],
+      test: '<',
+    },
+    { operands: [ '_object.relay', {_catalog: {name: 'getOtherSensorByUserId',
+                                               args: [{_value: '1234'}, {_value :1000}]}},
+                ],
+      test: '=='
+    },
+  ],
+  then:
+    {
+      _catalog : {name: 'setTemp',
+                  args: ['_object.temp']}
+    }
+};
+
+var shortCircuitRule = {
+  ifs : [
+    { operands: [ '_object.relay', {_catalog: {name: 'getOtherSensorByUserId',
+                                               args: [{_value: '1234'}, {_value :5000}]}},
+                ],
+      test: '=='
+    },
+    { operands: [ '_object.relay', {_catalog: {name: 'getOtherSensorByUserId',
+                                               args: [{_value: '1233'}, {_value :1000}]}},
+                ],
+      test: '=='
+    },
+  ],
+  then:
+    {
+      _catalog : {name: 'setTemp',
+                  args: ['_object.temp']}
+    }
+};
+
+
+var getOtherSensorByUserId = function(userid, timeout,cb){
+  setTimeout(function(){
+    if(userid === '1234')
+       cb(null, 1);
+    else
+      cb(null, false);
+  }, timeout);
+};
+
+
 describe("Json Rules Constructor", function(){
   it("return a function", function(done){
     var ruleEngine = new JsonRules();
@@ -86,6 +135,37 @@ describe("JsonRules.doRule", function(){
     ruleEngine.doRule(exampleRule, value2, function(err, fn){
       assert.equal(fn, null);
       done();
+    });
+  });
+  it("tests a rule with an catalog attribute and returns fn because it passes ", function(done){
+    this.timeout(1200); 
+    var exampleThen = function(word){return "example"}
+    var catalog = new FnCatalog();
+    catalog.addFn(exampleThen, 'setTemp', this);
+    catalog.addFn(getOtherSensorByUserId, 'getOtherSensorByUserId', this);
+    var ruleEngine = new JsonRules({catalog: catalog});
+    ruleEngine.doRule(catalogOperandRule, value1, function(err, fn){
+      if(fn){
+        assert.equal(fn(), exampleThen());
+        done();
+      }else{
+        throw("Should have received Then but didn't"); 
+      }
+    });
+  });
+  it("tests a rule with an catalog attribute and returns a null fn after the first false if statement fails", function(done){
+    this.timeout(1200); 
+    var exampleThen = function(word){return "example"}
+    var catalog = new FnCatalog();
+    catalog.addFn(exampleThen, 'setTemp', this);
+    catalog.addFn(getOtherSensorByUserId, 'getOtherSensorByUserId', this);
+    var ruleEngine = new JsonRules({catalog: catalog});
+    ruleEngine.doRule(shortCircuitRule, value1, function(err, fn){
+      if(fn){
+        throw("Shouldn't have returned a function"); 
+      }else{
+        done();
+      }
     });
   });
 });
